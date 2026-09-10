767e5200d34875280d22b700267ed5ab8b2d96f7
Christian <git@ctroller.com>
2026-09-10 16:27:25 +0200

    fix: Queue should use FileStore as much as possible

--- apps/file-converter/internal/task/queue.go
+++ apps/file-converter/internal/task/queue.go
@@ -68,9 +68,7 @@ func (q *Queue) process(ctx context.Context, job Job) {
 		slog.Error("failed to process job", "id", job.ID, "err", err)
 		q.store.Set(job.ID, JobResult{JobID: job.ID, Status: StatusFailed, Err: err})
 		if name != "" {
-			if err := os.Remove(name); err != nil {
-				slog.Warn("failed to remove file", "err", err)
-			}
+			q.fileStore.Delete(name)
 		}
 		return
 	}
